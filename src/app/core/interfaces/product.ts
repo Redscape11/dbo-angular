39f6727458b2796d380b82c90e2771f944c192aa
Redscape11 <redangelrs11@gmail.com>
2022-01-15 17:16:39 +0100

    increment/decrement stock and error message

--- src/app/core/interfaces/product.ts
+++ src/app/core/interfaces/product.ts
@@ -1,8 +1,9 @@
 export interface Product {
   id?: number,
+  tig_id?: number,
   name?: string,
   discount?: number,
   availability?: boolean,
   price?: number,
-  quantity_stock?: number,
+  quantityInStock?: number,
 }
